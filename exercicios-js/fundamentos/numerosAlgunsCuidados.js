bba4363d718ab453b2c3f24f92853dc8e8fa24b1
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-02-17 20:08:40 -0300

    até boolean

---
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7 / 0) //qualquer numero divido por 0 da infinito (infinity)
+console.log(7 / 0.00000000001)
+console.log('10' / 2)
+console.log("show!" * 2)
+console.log(0.1 + 0.7)
+//console.log(10.toString()) //não funciona
+console.log((10.351).toFixed(3))
